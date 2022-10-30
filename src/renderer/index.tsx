3224f1a9f8f3b06c58b90406c901ecdb1e86d6c4
RedCrafter07 <gh@r07.dev>
2022-10-30 21:29:19 +0100

    Use wrappers instead of app

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import '@fontsource/figtree';
+import Wrappers from './wrappers';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(<Wrappers />);
