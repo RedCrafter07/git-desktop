ccd181d7a6c24684cb26eb994fd17ad17cedde81
RedCrafter07 <gh@r07.dev>
2022-10-31 20:37:24 +0100

    Import font

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import '@fontsource/figtree';
+import '@fontsource/source-code-pro';
 import Wrappers from './wrappers';
 
 const container = document.getElementById('root')!;
