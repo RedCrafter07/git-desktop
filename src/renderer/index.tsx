9889aacbcd29efece118697ea753b5c47e4b8e39
RedCrafter07 <gh@r07.dev>
2022-11-03 17:13:38 +0100

    Import roboto

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import '@fontsource/figtree';
 import '@fontsource/source-code-pro';
+import '@fontsource/roboto';
 import Wrappers from './wrappers';
 
 const container = document.getElementById('root')!;
